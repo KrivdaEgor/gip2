425a2a856d348db17d369003256f5d39f674efeb
Allan Rasson <r0672070@ucll.be>
2020-04-07 00:47:20 +0200

    models: user added view login and index

---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GIP2.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string role { get; set; }
+
+    }
+}
